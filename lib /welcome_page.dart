f67d90b68e835896dce259e5a8ae4ba74acbde13
jana-alzhrani <janafahdalzhrani@gmail.com>
2026-04-11 01:31:46 +0300

    Update welcome_page.dart

--- lib /welcome_page.dart
+++ lib /welcome_page.dart
@@ -7,6 +7,7 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppDesign.background,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30),
